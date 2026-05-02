20399cf0adbe834838f2fed8b97c437951eb45f5
Ivan Naumann Thostrup <naumannthostrupivan@gmail.com>
2026-05-02 20:05:26 -0300

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './shared/components/top-bar/top-bar.component';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
-import { InfoServiciosComponent } from "./pages/info-servicios/info-servicios.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBarComponent, NavBarComponent, InfoServiciosComponent, FooterComponent],
+  imports: [RouterOutlet, TopBarComponent, NavBarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
