22a026ea6b7c14138acd91fe84e5cb4c848f594d
ju1010 <alvaroleonelaguirre@hotmail.com>
2026-05-04 00:33:19 -0300

    implementacion de sponsor-bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './shared/components/top-bar/top-bar.component';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 import { FooterComponent } from "./shared/components/footer/footer.component";
+import { SponsorBarComponent } from './shared/components/sponsor-bar/sponsor-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBarComponent, NavBarComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    TopBarComponent,
+    NavBarComponent,
+    FooterComponent,
+    SponsorBarComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
