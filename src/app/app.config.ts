649e6cf385422ed6f498014c4e3a896fa2b6e1a7
Ivan Naumann Thostrup <naumannthostrupivan@gmail.com>
2026-05-11 14:50:16 -0300

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: APP_BASE_HREF, useValue: environment.baseHref }
   ]
 };
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseHref: '/2026/'
+};
